6ed76f5e4e5c3461ba34beb2608eb1fe3db8c449
Aleksandr Zhirkevich <sasha.zhirkevich@gmail.com>
2022-06-15 00:34:14 +0300

    logo shape affects on logo bitmap (not only padding)

--- custom_qr_generator/src/main/java/com/github/alexzhirkevich/customqrgenerator/QRGenerator.kt
+++ custom_qr_generator/src/main/java/com/github/alexzhirkevich/customqrgenerator/QRGenerator.kt
@@ -65,6 +65,7 @@ class QrGenerator : QrCodeGenerator {
                     if (!isActive()){
                         throw CancellationException()
                     }
+
                     val color = backgroundBitmap[x, y].toColorLong()
 
                     val bgImageColor = Color.argb(
@@ -192,6 +193,9 @@ class QrGenerator : QrCodeGenerator {
             for (i in 0 until bitmapLogo.width) {
                 for (j in 0 until bitmapLogo.height) {
 
+                    if (!options.logo.shape.invoke(i,j,bitmapLogo.width, bitmapLogo.width, Neighbors.Empty)){
+                        continue
+                    }
                     if (!isActive()) {
                         throw CancellationException()
                     }
