dfb0b3e7ccab0f7a66a84a83477aa7a37ad10c28
Aleksandr Zhirkevich <sasha.zhirkevich@gmail.com>
2023-01-22 19:32:18 +0300

    added javadoc that describes issue #15

--- custom_qr_generator/src/main/java/com/github/alexzhirkevich/customqrgenerator/vector/style/QrVectorLogoPadding.kt
+++ custom_qr_generator/src/main/java/com/github/alexzhirkevich/customqrgenerator/vector/style/QrVectorLogoPadding.kt
@@ -1,6 +1,7 @@
 package com.github.alexzhirkevich.customqrgenerator.vector.style
 
 import com.github.alexzhirkevich.customqrgenerator.SerializationProvider
+import com.github.alexzhirkevich.customqrgenerator.style.QrLogoPadding.Accurate
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.SerializationStrategy
@@ -8,20 +9,50 @@ import kotlinx.serialization.modules.SerializersModule
 import kotlinx.serialization.modules.polymorphic
 import kotlinx.serialization.modules.subclass
 
+/**
+ * Type of padding applied to the logo.
+ * Padding applied even if logo drawable is not specified.
+ *
+ * Prefer empty padding if your qr code encodes large amount of data
+ * to avoid performance issues.
+ * */
 sealed interface QrVectorLogoPadding {
 
     val value : Float
 
+
+    /**
+     * Logo will be drawn on top of QR code without any padding.
+     * QR code pixels might be visible through transparent logo.
+     *
+     * Prefer empty padding if your qr code encodes large amount of data
+     * to avoid performance issues.
+     * */
     @kotlinx.serialization.Serializable
     @SerialName("Empty")
     object Empty : QrVectorLogoPadding {
         override val value: Float get() = 0f
     }
 
+
+    /**
+     * Padding will be applied directly according to the shape of logo.
+     * Some QR code pixels can be cut.
+     *
+     * WARNING: this padding can cause performance issues for qr codes with
+     * large amount out data
+     * */
     @kotlinx.serialization.Serializable
     @SerialName("Accurate")
     data class Accurate(override val value: Float) : QrVectorLogoPadding
 
+
+    /**
+     * Works like [Accurate] but all clipped pixels will be removed.
+     *
+     * This padding can also cause a little performance issues wen applied
+     * to large-data qr codes, but not as much as [Accurate].
+     * */
     @kotlinx.serialization.Serializable
     @SerialName("Natural")
     data class Natural(override val value: Float) : QrVectorLogoPadding
